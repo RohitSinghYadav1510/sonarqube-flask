65018a981d36c6f9120e0af202a013e6dce1f674
Rohit Singh yadav <yadavrohit1510@gmail.com>
2021-09-27 19:52:55 +0530

    ef

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import asyncio
 import time
 from random import randint
-import https
+import httpx
 from flask import Flask
 
 
@@ -17,7 +17,7 @@ async def get_xkcd_image(session):
 
 # function converted to coroutine
 async def get_multiple_images(number):
-    async with https.AsyncClient() as session: # async client used for async functions
+    async with httpx.AsyncClient() as session: # async client used for async functions
         tasks = [get_xkcd_image(session) for _ in range(number)]
         result = await asyncio.gather(*tasks, return_exceptions=True) # gather used to collect all coroutines and run them using loop and get the ordered response
     return result
